9794c56c677d981e2df5eec60717d57c565c1b86
Vladimir Dymov <lebed19732212@gmail.com>
2022-12-08 12:33:23 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/888777.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "888777.exe"
+  "888777.exe.manifest"
+  "888777.pdb"
+  "CMakeFiles/888777.dir/main.cpp.obj"
+  "lib888777.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/888777.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
